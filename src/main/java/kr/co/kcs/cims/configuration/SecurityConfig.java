5aa4d26e0d4271d29577901d8e2aea860bc8f7f7
Daekwon Kang <daekwon.kang@gmail.com>
2024-12-27 10:13:52 +0900

    [SDEV-250] hello 엔드포인트 추가

---
+++ src/main/java/kr/co/kcs/cims/configuration/SecurityConfig.java
@@ -0,0 +1,26 @@
+package kr.co.kcs.cims.configuration;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/hello")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .formLogin(withDefaults());
+
+        return http.build();
+    }
+}
---
+++ src/main/java/kr/co/kcs/cims/web/HelloController.java
@@ -0,0 +1,12 @@
+package kr.co.kcs.cims.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String index() {
+        return "hello";
+    }
+}
