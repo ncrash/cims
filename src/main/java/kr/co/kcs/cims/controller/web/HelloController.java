cfaf912da0ed3c7a2ebbff5319c6d2de71fdea4c
Daekwon Kang <daekwon.kang@gmail.com>
2024-12-30 16:49:13 +0900

    [SDEV-250] index page 대응처리, swagger url 업데이트

--- src/main/java/kr/co/kcs/cims/controller/web/HelloController.java
+++ src/main/java/kr/co/kcs/cims/controller/web/HelloController.java
@@ -2,13 +2,19 @@ package kr.co.kcs.cims.controller.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import io.swagger.v3.oas.annotations.Hidden;
 
 @Hidden
 @RestController
 public class HelloController {
-    @GetMapping({"/", "/hello"})
+    @GetMapping("/")
+    public ModelAndView root() {
+        return new ModelAndView("redirect:/kcs/swagger.html");
+    }
+
+    @GetMapping("/hello")
     public String index() {
         return "hello";
     }
